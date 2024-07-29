a71be02eaeb2f4dc61054071a24307ca4e53c321
wombatwebcg <wombatwebcg@gmail.com>
2024-07-29 17:51:54 +0800

    修改程序加载的缺少dll的bug

--- Wombat.DependencyInjection/AssemblyLoader.cs
+++ Wombat.DependencyInjection/AssemblyLoader.cs
@@ -43,6 +43,7 @@ namespace Wombat.DependencyInjection
                     result.Add(entryAssembly);
                 }
 
+
                 IEnumerable<AssemblyName> source2 = entryAssembly.GetReferencedAssemblies()
                     .Where(w => !ignoreList.Any(s => (w.Name ?? "").Contains(s)))
                     .Where(w => !result.Any(s => (w.Name ?? "").Contains(s.GetName().Name ?? "")));
@@ -54,9 +55,9 @@ namespace Wombat.DependencyInjection
                 result.AddRange(list);
 
                 IEnumerable<string> enumerable = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory)
-                    .Where(w => w.EndsWith(".dll"))
-                    .Where(w => !ignoreList.Any(s => Path.GetFileName(w)?.Contains(s) ?? false))
-                    .Where(w => !result.Any(s => s?.Location == w));
+                .Where(w => w.ToLower().EndsWith(".dll"))
+                .Where(w => !ignoreList.Any(s => Path.GetFileName(w)?.Contains(s) ?? false))
+                .Where(w => !result.Any(s => s?.Location == w));
 
                 if (assemblyNames != null && assemblyNames.Length != 0)
                 {
@@ -69,20 +70,25 @@ namespace Wombat.DependencyInjection
                     {
                         continue;
                     }
-
-                    try
+                    else
                     {
-                        Assembly assembly = LoadAssemblyByName(item);
-                        if (!result.Any(w => w?.FullName == assembly?.FullName))
+                        try
                         {
-                            result.Add(assembly);
+                            Assembly assembly = Assembly.LoadFrom(item);
+                            if (!result.Any(w => w?.FullName == assembly?.FullName))
+                            {
+                                result.Add(assembly);
+                            }
+
+                        }
+                        catch (Exception)
+                        {
+
                         }
 
-                    }
-                    catch (Exception)
-                    {
 
                     }
+
                 }
 
                 return result;
@@ -157,7 +163,7 @@ namespace Wombat.DependencyInjection
         {
             var loadedAssemblies = new List<Assembly>();
             var loadedAppDomainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
-            Assembly.GetEntryAssembly().GetReferencedAssemblies().ForEach((name) => { AddIfNotAlreadyAdded(loadedAssemblies, Assembly.Load(name)); }) ;
+            Assembly.GetEntryAssembly().GetReferencedAssemblies().ForEach((name) => { AddIfNotAlreadyAdded(loadedAssemblies, Assembly.Load(name)); });
             foreach (var assembly in loadedAppDomainAssemblies)
             {
                 AddIfNotAlreadyAdded(loadedAssemblies, assembly);
@@ -174,13 +180,53 @@ namespace Wombat.DependencyInjection
             return loadedAssemblies;
         }
 
-        private static void AddIfNotNull(List<Assembly> assemblies, Assembly assembly)
-        {
-            if (assembly != null)
-            {
-                assemblies.Add(assembly);
-            }
-        }
+
+
+
+        //private static IEnumerable<Assembly> GetLoadedAssemblies()
+        //{
+        //    var loadedAssemblies = new List<Assembly>();
+        //    var loadedAppDomainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        //    // 加载入口程序集的引用程序集
+        //    Assembly.GetEntryAssembly().GetReferencedAssemblies().ToList().ForEach(name =>
+        //    {
+        //        AddIfNotAlreadyAdded(loadedAssemblies, Assembly.Load(name));
+        //    });
+
+        //    // 加载当前AppDomain中的程序集
+        //    foreach (var assembly in loadedAppDomainAssemblies)
+        //    {
+        //        AddIfNotAlreadyAdded(loadedAssemblies, assembly);
+        //    }
+
+        //    // 加载入口程序集和当前执行程序集
+        //    var entryAssembly = Assembly.GetEntryAssembly();
+        //    var executingAssembly = Assembly.GetExecutingAssembly();
+        //    AddIfNotAlreadyAdded(loadedAssemblies, entryAssembly);
+        //    AddIfNotAlreadyAdded(loadedAssemblies, executingAssembly);
+
+        //    // 遍历指定路径下的DLL并加载
+        //    var dllFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll");
+        //    foreach (var dll in dllFiles)
+        //    {
+        //        try
+        //        {
+        //            var assembly = Assembly.LoadFrom(dll);
+        //            AddIfNotAlreadyAdded(loadedAssemblies, assembly);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            // 处理加载程序集失败的情况，例如记录日志
+        //            Console.WriteLine($"Failed to load assembly from {dll}: {ex.Message}");
+        //        }
+        //    }
+
+        //    // 移除动态程序集
+        //    loadedAssemblies.RemoveAll(i => i.IsDynamic);
+
+        //    return loadedAssemblies;
+        //}
 
         private static void AddIfNotAlreadyAdded(List<Assembly> assemblies, Assembly assembly)
         {
@@ -189,6 +235,18 @@ namespace Wombat.DependencyInjection
                 assemblies.Add(assembly);
             }
         }
+
+
+
+
+        private static void AddIfNotNull(List<Assembly> assemblies, Assembly assembly)
+        {
+            if (assembly != null)
+            {
+                assemblies.Add(assembly);
+            }
+        }
+
 
     }
 }
